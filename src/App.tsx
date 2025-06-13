763500c30b3cee2a237c861394650998bdfbd3f9
Thais Campos <thaisc.carvalho4@gmail.com>
2025-06-12 21:44:44 -0300

    Aula 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+export function App(){
+    console.log("Oie")
+    return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Olá, Mundo!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id faucibus neque. Pellentesque finibus nulla vitae 
+        congue ultricies. Curabitur placerat nec dolor non efficitur. Nunc viverra enim est, et congue ante suscipit quis. Praesent 
+        quis tortor venenatis, euismod libero vel, euismod ante. Morbi in eleifend ante, quis elementum mi. Sed eros neque, fermentum
+         ac vulputate vitae, feugiat sed massa. Aenean vel imperdiet tellus. Vestibulum tempus lacus ut bibendum tristique. Aenean et
+        dui varius, mattis orci sed, imperdiet ante. Suspendisse iaculis ornare finibus. Integer sed varius leo. Donec sed nisi tempus,
+         tempus libero bibendum, condimentum nisl. Mauris ultricies nec mauris vel maximus.</p>
     </>
-  )
+    )
 }
 
-export default App
